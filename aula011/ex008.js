2f8e30b62c6677439dff219aedc04a3b2d525cf6
Vini Moraes <81641780+vinis-moraes@users.noreply.github.com>
2023-03-26 22:20:49 -0300

    Aula 011 Updated

--- aula011/ex008.js
+++ aula011/ex008.js
@@ -1,2 +1,6 @@
-var vel = 60.5
+var vel = 80
 console.log(`A velocidade do seu carro é ${vel}km/h.`)
+if (vel > 60) { // Condição simples!
+    console.log('Você ultrapassou a velcidade permitida. MULTADO!')
+}
+console.log('Dirija sempre usando cinto de segurança!')
