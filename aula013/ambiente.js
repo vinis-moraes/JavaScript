656260602e3c0666d7e0c58b1fea85a5bda162ea
Vini Moraes <81641780+vinis-moraes@users.noreply.github.com>
2023-03-29 21:48:40 -0300

    Update

---
+++ aula013/ambiente.js
@@ -0,0 +1,14 @@
+// console.log('Tudo bem?')
+
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c ++
+} while (c <= 1024)
+
+
+/*var c = 1
+while (c <= 1024) {
+    console.log(`Passo ${c}`)
+    c ++
+}*/
