4930d426994aefe22034446df9748667d9557f6a
Vini Moraes <81641780+vinis-moraes@users.noreply.github.com>
2023-04-01 14:07:53 -0300

    Update

---
+++ aula015/ambiente.js
@@ -0,0 +1,22 @@
+var num = [5, 8, 2, 9, 3]
+
+num.push(1)
+
+num.sort()
+
+console.log(num)
+
+console.log(`O vetor tem ${num.length} posições`)
+
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+
+var num1 = 11
+
+var pos = num.indexOf(num1)
+
+if (pos == -1){
+    console.log(`O valor não foi encontrado`)
+} else{
+    console.log(`O valor ${num1} está na posição ${pos}`)
+}
+
